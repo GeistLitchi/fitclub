54c85e6b6c10865c366276e2d4ab5fadd2bdd30f
jaydo <dodhiajk@myumanitoba.ca>
2023-02-06 12:53:12 -0600

    added muscle group page and some code to go back and forward with parent class

--- app/src/main/java/comp3350/fitclub/presentation/MainActivity.java
+++ app/src/main/java/comp3350/fitclub/presentation/MainActivity.java
@@ -2,6 +2,7 @@ package comp3350.fitclub.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -15,11 +16,15 @@ import comp3350.fitclub.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button goToMuscleGroupBtn = (Button) findViewById(R.id.btn_go_to_muscle_group);
         Button goToExercisesBtn = (Button) findViewById(R.id.btn_go_to_exercises_activity);
         goToExercisesBtn.setOnClickListener(new View.OnClickListener()
         {
@@ -29,7 +34,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        goToMuscleGroupBtn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, MuscleGroupPage.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 
     @Override
     public void onDestroy(){super.onDestroy();}
---
+++ app/src/main/java/comp3350/fitclub/presentation/MuscleGroupPage.java
@@ -0,0 +1,79 @@
+package comp3350.fitclub.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import comp3350.fitclub.R;
+
+public class MuscleGroupPage extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muscle_group_page);
+
+        //colleting all the button in java file
+        Button btn_1 = findViewById(R.id.bicep_btn);
+        Button btn_2 = findViewById(R.id.legs_btn);
+        Button btn_3 = findViewById(R.id.abs_btn);
+        Button btn_4 = findViewById(R.id.back_btn);
+        Button btn_5 = findViewById(R.id.chest_btn);
+        Button btn_6 = findViewById(R.id.shoulder_btn);
+
+
+        //giving instance of each button to themselves for implementing on click function
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        String muscleName;                      // name of the muscle group you clicked
+        Button buttonClicked;                   // to store id of the button you clicked
+        switch(view.getId()){
+            case R.id.bicep_btn:
+                buttonClicked = findViewById(R.id.bicep_btn);
+                muscleName = buttonClicked.getText().toString();                            //getting the name of the text stored in buttonClicked
+                Toast.makeText(this, muscleName, Toast.LENGTH_SHORT).show();        //test output
+                break;
+            case R.id.legs_btn:
+                buttonClicked = findViewById(R.id.legs_btn);
+                muscleName = buttonClicked.getText().toString();                            //getting the name of the text stored in buttonClicked
+                Toast.makeText(this, muscleName, Toast.LENGTH_SHORT).show();        //test output
+                break;
+            case R.id.abs_btn:
+                buttonClicked = findViewById(R.id.abs_btn);
+                muscleName = buttonClicked.getText().toString();                            //getting the name of the text stored in buttonClicked
+                Toast.makeText(this, muscleName, Toast.LENGTH_SHORT).show();        //test output
+                break;
+            case R.id.back_btn:
+                buttonClicked = findViewById(R.id.back_btn);
+                muscleName = buttonClicked.getText().toString();                            //getting the name of the text stored in buttonClicked
+                Toast.makeText(this, muscleName, Toast.LENGTH_SHORT).show();        //test output
+                break;
+            case R.id.chest_btn:
+                buttonClicked = findViewById(R.id.chest_btn);
+                muscleName = buttonClicked.getText().toString();                            //getting the name of the text stored in buttonClicked
+                Toast.makeText(this, muscleName, Toast.LENGTH_SHORT).show();        //test output
+                break;
+            case R.id.shoulder_btn:
+                buttonClicked = findViewById(R.id.shoulder_btn);
+                muscleName = buttonClicked.getText().toString();                            //getting the name of the text stored in buttonClicked
+                Toast.makeText(this, muscleName, Toast.LENGTH_SHORT).show();        //test output
+                break;
+
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + view.getId());
+        }
+    }
+}
