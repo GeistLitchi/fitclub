2561128f59e7e6ad21efa064be72f58d55d2a59f
Will Sutherland <suther15@myumanitoba.ca>
2023-02-09 23:24:58 -0600

    Start of Workout class

--- app/src/main/java/comp3350/fitclub/objects/Workout.java
+++ app/src/main/java/comp3350/fitclub/objects/Workout.java
@@ -1,4 +1,35 @@
 package comp3350.fitclub.objects;
 
+import java.util.ArrayList;
+
 public class Workout {
+    /*--------- instance variables ------------*/
+    private String name;    //name of the workout
+//    private ArrayList<Exercise> exerciseList; //list of exercises in the workout
+
+    /*--------- constructors ------------*/
+    //default constructor
+    public Workout() {
+//        ArrayList<Exercise> exerciseList = new ArrayList<Exercise>();
+    }
+
+    public Workout(String name) {
+//        ArrayList<Exercise> exerciseList = new ArrayList<Exercise>();
+        this.name = name;
+    }
+
+    /*---------- instance methods ------------*/
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+//        return exerciseList.size();
+        return 0;
+    }
+
+    //set name for creation of a custom workout
+    public void setName(String newName) {
+        name = newName;
+    }
 }
