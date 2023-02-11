b9df119b18a6703cc64617c40cafa096b61fbaa5
Will Sutherland <suther15@myumanitoba.ca>
2023-02-10 20:12:26 -0600

    fixing up Workout object

--- app/src/main/java/comp3350/fitclub/objects/Workout.java
+++ app/src/main/java/comp3350/fitclub/objects/Workout.java
@@ -5,17 +5,23 @@ import java.util.ArrayList;
 public class Workout {
     /*--------- instance variables ------------*/
     private String name;    //name of the workout
-//    private ArrayList<Exercise> exerciseList; //list of exercises in the workout
+    private String type;    //the type of workout
+    private int difficulty; //value between 1-3 meaning simple, medium, difficult to match exercise difficulty
+    private ArrayList<Exercise> workoutExercises; //list of exercises in the workout
 
     /*--------- constructors ------------*/
-    //default constructor
-    public Workout() {
-//        ArrayList<Exercise> exerciseList = new ArrayList<Exercise>();
+    public Workout(String name) {
+        workoutExercises = new ArrayList<Exercise>();
+        this.name = name; //in future check if name is in db already?
+        type = null; //can change this to custom in later implementations
+        difficulty = 0;
     }
 
-    public Workout(String name) {
-//        ArrayList<Exercise> exerciseList = new ArrayList<Exercise>();
+    public Workout(String name, String type, int difficulty) {
+        workoutExercises = new ArrayList<Exercise>();
         this.name = name;
+        this.type = type;
+        this.difficulty = difficulty;
     }
 
     /*---------- instance methods ------------*/
@@ -23,13 +29,31 @@ public class Workout {
         return name;
     }
 
-    public int getSize() {
-//        return exerciseList.size();
-        return 0;
+    public int getDifficulty() {
+        return difficulty;
     }
 
-    //set name for creation of a custom workout
-    public void setName(String newName) {
-        name = newName;
+    public String getType() {
+        return type;
+    }
+
+    public int getSize() {
+        return workoutExercises.size();
+    }
+
+    public String toString() {
+        return String.format("Workout: %s (%s)", name, type);
+    }
+
+    public boolean equals(Object other) {
+        boolean isEqual = false;
+
+        if(other instanceof Workout) {
+            final Workout otherWorkout = (Workout) other;
+
+            isEqual = this.name.equals(otherWorkout.name);
+        }
+
+        return isEqual;
     }
 }
