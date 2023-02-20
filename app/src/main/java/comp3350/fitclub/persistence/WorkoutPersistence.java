8abb25940022f7a166f42a8d8b8cfa663a6ce560
Will Sutherland <suther15@myumanitoba.ca>
2023-02-19 20:09:47 -0600

    clean up workoutDB

--- app/src/main/java/comp3350/fitclub/persistence/WorkoutPersistence.java
+++ app/src/main/java/comp3350/fitclub/persistence/WorkoutPersistence.java
@@ -1,14 +1,12 @@
 package comp3350.fitclub.persistence;
 
 import comp3350.fitclub.objects.Workout;
-import java.util.ArrayList;
+import java.util.List;
 
 public interface WorkoutPersistence {
-    ArrayList<Workout> getAllWorkouts();
+    List<Workout> getAllWorkouts();
 
     Workout insertWorkout(Workout current);
 
     Workout updateWorkout(Workout current);
-
-    void deleteWorkout(Workout current);
 }
